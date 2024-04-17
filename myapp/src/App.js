b5bb4444b6d4ad1c239d7cecf25f95a3ecedd2ac
sumitnegikv369 <sumit.negi@successive.tech>
2024-04-17 16:33:42 +0530

    resolve conflict

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import Assignment1 from './day1/Assignment1'
 import Assignment2 from './day2/Assignment2'
+import Assignment3 from './day3/Assignment3'
 import Assignment5 from './day5/Assignment5'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
       {/* <Assignment1 /> */}
       {/* <Assignment2 /> */}
+      {/* <Assignment3 /> */}
       <Assignment5/>
     </>
   )
